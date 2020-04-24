1e63382566dee174d5ccae0b560fe52ebc180f41
taishi <taishi@cutput.com>
2020-04-24 23:01:29 +0800

    add build

---
+++ build/precache-manifest.11874ee74731289c583fe743c1322777.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf165218a3ff194cd8814fd0b7e59a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75fa86645b4888e69ef",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "b4d9576aa1b0dc1bbff1",
+    "url": "/static/css/main.70502144.chunk.css"
+  },
+  {
+    "revision": "b75fa86645b4888e69ef",
+    "url": "/static/js/2.5e13a991.chunk.js"
+  },
+  {
+    "revision": "7e8661572dbf62ca66d7837115f59618",
+    "url": "/static/js/2.5e13a991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d9576aa1b0dc1bbff1",
+    "url": "/static/js/main.3643dcee.chunk.js"
+  },
+  {
+    "revision": "a84073ad803d02e46558",
+    "url": "/static/js/runtime-main.97b83d82.js"
+  },
+  {
+    "revision": "1371271218ac62e375ea668b2a852b2d",
+    "url": "/static/media/index.13712712.less"
+  }
+]);
